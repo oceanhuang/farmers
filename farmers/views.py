47cf8c48f5e9a5655788970195629590d259f3f5
Alex <amcguiga@gmail.com>
2015-01-30 11:45:00 -0500

    fixed some html issues

--- farmers/views.py
+++ farmers/views.py
@@ -6,15 +6,8 @@ from django.template import RequestContext, loader
 
 class HomePageView(TemplateView):
     template_name = 'base.html'
+
     
-    cat = category
-    everything = Product.objects.all()
-    prodSet = []
-    for produce in everything:
-        if produce.category == cat:
-            if len(produce.getFarmers(date)) >= 1:
-                prodSet.append(produce)
-    return HttpResponse(prodSet)
 
 
 class CheckoutPageView(TemplateView):
