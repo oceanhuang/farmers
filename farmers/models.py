5fd6522bf61d5fcba990ea10740fb28d8ed02d60
Huang <e050862@c02ngj71g3qc.corp.mastercard.org>
2015-01-30 02:12:04 -0500

    added some views and templates

--- farmers/models.py
+++ farmers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import libs.postmates as pm
 
 class Product(models.Model):
 	category = models.CharField(max_length=200)
@@ -23,3 +24,13 @@ class Farmer(models.Model):
 class Availability(models.Model):
 	farmers = models.ForeignKey(Farmer)
 	date = models.DateField(auto_now=False)
+
+'''
+api = pm.PostmatesAPI("f8aa101d-6bc5-400d-96a1-ac7ee4a38c28", "cus_KBZUGmEchkNykk")
+
+pickup = pm.Location('Alice', 'Union Square, New York, NY 10011', '415-555-0000')
+dropoff = pm.Location('Bob', '301 w 110th st, New York, NY 10026', '415-777-9999')
+
+quote = pm.DeliveryQuote(api, pickup.address, dropoff.address)
+print quote
+'''
