5c8c9145271c2091f0c12faec24fb2b12d521d7c
Alex <amcguiga@gmail.com>
2015-01-30 07:27:59 -0500

    changes to views

--- farmers/views.py
+++ farmers/views.py
@@ -1,9 +1,20 @@
 from django.views.generic.base import TemplateView
+from farmers.models import Product
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 
 class HomePageView(TemplateView):
     template_name = 'base.html'
 
+    cat = category
+    everything = Product.objects.all()
+    prodSet = []
+    for produce in everything:
+        if produce.category == cat:
+            if len(produce.getFarmers(date)) >= 1:
+                prodSet.append(produce)
+    return HttpResponse(prodSet)
 
 
 
