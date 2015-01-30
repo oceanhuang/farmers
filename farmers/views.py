a9d795748b1377cfa54241d4e1cf69bb6c8f7aff
Huang <e050862@c02ngj71g3qc.corp.mastercard.org>
2015-01-30 06:51:07 -0500

    init page

--- farmers/views.py
+++ farmers/views.py
@@ -4,8 +4,10 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = 'base.html'
 
+class CheckoutPageView(TemplateView):
+	template_name = 'checkout.html'
 
-
-
+class InitView(TemplateView):
+	template_name = 'init.html'
 
 
