336779e8ae6a64afcce3a3bfa38922f318c7a073
McGuigan <e045873@c02nghssg3qc.corp.mastercard.org>
2015-01-30 01:02:58 -0500

    fixture and some migrating

--- farmers/models.py
+++ farmers/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
 class Farmer(models.Model):
 	name = models.CharField(max_length=200)
 	description = models.CharField(max_length=200)
+	phone = models.IntegerField(default=11111111111, max_length=10)
 
 	products = models.ManyToManyField(Product)
 
